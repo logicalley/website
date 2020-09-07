48f7bd1a1202fde394c137c0bda058f5b3ce3b1c
Bolaji Olajide <cooproton@gmail.com>
2020-09-07 21:11:58 +0100

    configure next

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// @ts-check
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = {
+  pageExtensions: ['tsx', 'ts']
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import React from 'react';
 
-const Home: React.FC  = (): any => {
+const Home: React.FC  = () => {
   return (
     <div>
       <p>welcomeText</p>
