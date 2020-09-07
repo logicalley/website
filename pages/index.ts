53c0e931f9f22eae6621c716446ada99238c89c1
Bolaji Olajide <cooproton@gmail.com>
2020-09-07 20:37:12 +0100

    initial commit

---
+++ pages/index.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const welcomeText: string = "Welcome to Annie";
+const Home: React.FC  = () => (
+  <div>
+    <p>{welcomeText}</p>
+  </div>
+);
+
+export default Home;
