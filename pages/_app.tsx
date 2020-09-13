e145b7e2daf2cb7450f1553e0c5d2789fcf7a2dd
Bolaji Olajide <cooproton@gmail.com>
2020-09-13 12:19:29 +0100

    feat: display favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import React, { Fragment } from 'react';
+import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 
 import '../assets/css/global.css';
 
 
-const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <Fragment>
+    <Head>
+      <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+      <title>Annie</title>
+    </Head>
+    <Component {...pageProps} />
+  </Fragment>
+);
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import React, { Fragment } from 'react';
-import Head from 'next/head';
+
 
 const Home: React.FC = () => {
   return (
     <Fragment>
-      <Head>
-        <title>Annie</title>
-      </Head>
       <section>
         <p>welcomeText</p>
       </section>
