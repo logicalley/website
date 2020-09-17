13de8113e19d96926d49004cb4743c58a837f726
Bolaji Olajide <cooproton@gmail.com>
2020-09-17 16:03:12 +0100

    feat: create base annie track view

---
+++ pages/track/[id].tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import Head from 'next/head';
+
+
+const TrackDetail: React.FC = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>How to love | Annie</title>
+      </Head>
+      <p>Track is showing!</p>
+    </Fragment>
+  );
+};
+
+export default TrackDetail;
