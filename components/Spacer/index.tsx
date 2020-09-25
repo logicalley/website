4bfb71c919cb5c35d64814ae495d0c97015d1220
Bolaji Olajide <cooproton@gmail.com>
2020-09-25 04:22:17 +0100

    feat: make spacer component responsive

--- components/Spacer/index.tsx
+++ components/Spacer/index.tsx
@@ -3,10 +3,17 @@ import React, { useEffect, useState, memo } from 'react';
 import type { SpacerProps } from '../..';
 
 const Spacer: React.FC<SpacerProps> = ({ h, mh }: SpacerProps) => {
+  console.log(process.browser)
   const [height, setHeight] = useState(h);
+
   useEffect(() => {
-    const heightToUse = window.innerWidth > 769 ? h : mh;
-    setHeight(heightToUse);
+    const handleResize = () => {
+      const heightToUse = window.innerWidth < 768 ? mh : h;
+      setHeight(heightToUse);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return <div style={{ height, width: '100%' }}></div>;
