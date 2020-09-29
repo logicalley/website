72e949275436ae58dac526f9779432b0f39f1401
Bolaji Olajide <cooproton@gmail.com>
2020-09-29 17:22:38 +0100

    feat: create shared SEO component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,25 @@ import { NextPage } from 'next';
 // components
 import Header from '../components/Header';
 import HomePage from '../components/HomePage';
+import SEO from '../components/SEO';
 
 
 const Home: NextPage = () => {
+  const pageTitle: string = 'Annie | Home';
+  const description: string = 'Annie helps you share your favorite songs and playlists between music platforms.';
+  const url: string = 'https://anniemusic.app';
+  const keywords: string = 'music song annie share spotify deezer apple sharing';
+  const imageUrl: string = 'https://res.cloudinary.com/anniemusic/image/upload/f_auto,q_auto/v1601396354/landing-page-assets/annie-logo_ruc0bw.png';
+
   return (
     <Fragment>
+      <SEO
+        title={pageTitle}
+        description={description}
+        url={url}
+        imageUrl={imageUrl}
+        keywords={keywords}
+      />
       <Header />
       <HomePage />
     </Fragment>
