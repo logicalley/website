c6cc8d6444781440d8657a0c5339c2444944b603
Bolaji Olajide <cooproton@gmail.com>
2020-09-30 22:44:54 +0100

    feat: update generic SEO values

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,14 @@ const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
       <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff"></meta>
+      <link rel="canonical" href="https://anniemusic.app" />
+      <meta name="twitter:site" content="@anniemusicapp" />
+      <meta name="twitter:creator" content="@anniemusicapp" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Annie Music" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="google-site-verification" content="" />
       <title>Annie</title>
     </Head>
     <Component {...pageProps} />
