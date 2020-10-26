b24c1daa06d944dee8036757a226b93b3033d643
Bolaji Olajide <cooproton@gmail.com>
2020-10-26 01:50:00 +0100

    feat: revamp beta url structure

---
+++ pages/beta/feedback.tsx
@@ -0,0 +1,29 @@
+import { Fragment } from 'react';
+import { NextPage } from 'next';
+
+import Header from '../../components/Header';
+import SEO from '../../components/SEO';
+import Footer from '../../components/Footer';
+import IFrameLoader from '../../components/IFrameLoader';
+
+const BetaFeedbackPage: NextPage = () => {
+  const pageTitle: string = 'Beta Volunteer Form';
+  const url: string = 'https://anniemusic.app/beta/volunteer';
+  const keywords: string = 'music song annie share spotify deezer apple sharing faq question answers';
+  const volunteerUrl: string = 'https://airtable.com/embed/shreOBdjoNCqglpKe?backgroundColor=orange';
+
+  return (
+    <Fragment>
+      <SEO
+        title={pageTitle}
+        url={url}
+        keywords={keywords}
+      />
+      <Header />
+      <IFrameLoader url={volunteerUrl} />
+      <Footer />
+    </Fragment>
+  )
+};
+
+export default BetaFeedbackPage;
