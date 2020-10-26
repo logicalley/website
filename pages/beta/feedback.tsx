823b5af0d83f6f3c93d5f94e2f569972fb06418f
Bolaji Olajide <cooproton@gmail.com>
2020-10-26 01:53:46 +0100

    feat: add SEO description to beta pages

--- pages/beta/feedback.tsx
+++ pages/beta/feedback.tsx
@@ -9,8 +9,9 @@ import IFrameLoader from '../../components/IFrameLoader';
 const BetaFeedbackPage: NextPage = () => {
   const pageTitle: string = 'Beta Feedback Form';
   const url: string = 'https://anniemusic.app/beta/volunteer';
-  const keywords: string = 'music song annie share spotify deezer apple sharing faq question answers';
+  const keywords: string = 'music song annie share spotify deezer apple sharing faq question answers beta';
   const volunteerUrl: string = 'https://airtable.com/embed/shrY7dnnae4yINho3?backgroundColor=purple';
+  const description: string = 'Share your feedback about the Annie Beta test.'
 
   return (
     <Fragment>
@@ -18,6 +19,7 @@ const BetaFeedbackPage: NextPage = () => {
         title={pageTitle}
         url={url}
         keywords={keywords}
+        description={description}
       />
       <Header />
       <IFrameLoader url={volunteerUrl} />
