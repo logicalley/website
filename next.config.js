f68b7db40b52a2faaa9e3f9b6ae1e3b728b5dc1b
Bolaji Olajide <cooproton@gmail.com>
2021-03-28 00:30:32 +0100

    setup csp in report mode

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const { createSecureHeaders } = require('next-secure-headers');
+const SriPlugin = require('webpack-subresource-integrity');
+
 // @ts-check
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -11,5 +14,60 @@ module.exports = {
   publicRuntimeConfig: {
     isDev,
     apiBaseUrl: process.env.ANNIE_API_BASE_URL
+  },
+  async headers() {
+    const headers = createSecureHeaders({
+      contentSecurityPolicy: {
+        reportOnly: true,
+        directives: {
+          defaultSrc: [
+            "'self'",
+            '*.dzcdn.net',
+            'api.anniemusic.app',
+            'localhost:5000/api/v1',
+            'data:'
+          ],
+          styleSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          fontSrc: ["'self'"],
+          imgSrc: [
+            "'self'",
+            'res.cloudinary.com',
+            '*.scdn.co',
+            '*.dzcdn.net',
+            'data:image/png;base64',
+            'data:'
+          ],
+          frameSrc: ['airtable.com'],
+          reportURI: `${process.env.ANNIE_API_BASE_URL}/complaint/csp`,
+          frameAncestors: ['airtable.com']
+        },
+      },
+      forceHTTPSRedirect: [
+        true,
+        {
+          maxAge: 60 * 60 * 24 * 4,
+          includeSubDomains: true
+        }
+      ],
+      referrerPolicy: 'same-origin',
+      frameGuard: false,
+      xssProtection: 'block-rendering'
+    });
+
+    console.log(headers);
+    return [{ source: '/(.*)', headers }];
+  },
+  poweredByHeader: false,
+  webpack: (config) => {
+    config.output.crossOriginLoading = 'anonymous';
+    config.plugins.push(
+      new SriPlugin({
+        hashFuncNames: ['sha256', 'sha384'],
+        enabled: false,
+      })
+    );
+
+    return config;
   },
 };
