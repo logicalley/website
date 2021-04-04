a83cd0831052f1f7f3029e5ae7e3e3729d894031
Bolaji Olajide <cooproton@gmail.com>
2021-04-04 05:45:11 +0100

    feat: update the privacy policy

--- components/PrivacyPolicy/index.tsx
+++ components/PrivacyPolicy/index.tsx
@@ -7,12 +7,12 @@ const PrivacyPolicy: React.FC = () => {
   return (
     <section className={styles.privacyPolicyContainer}>
       <h1 className={styles.privacyPolicyHeader}>Privacy Policy</h1>
+      <span className={styles.effectiveDate}>Effective Date: 4th of April, 2021</span>
       <p>
         Your privacy is important to us, we are committed to protecting your privacy.
-        The application does not collect or send any kind of personally identifiable information, except
-        for information required to create a profile within our application. We only ask for information
-        when we truly need it to provide a service to you. We collect it by fair and lawful means, with your
-        knowledge and consent.
+        We do not collect or send any kind of personally identifiable information, except for information required to create a profile within on Annie and for analytics.
+        We only ask for information when we truly need it to provide a service to you.
+        We collect it by fair and lawful means, with your knowledge and consent.
       </p>
 
       <p>
@@ -44,7 +44,7 @@ const PrivacyPolicy: React.FC = () => {
       </p>
 
       <p>
-        Our app may link to external sites that are not operated by us.
+        Anniemusic may link to external sites that are not operated by us.
         Please be aware that we have no control over the content and practices of
         these sites, and cannot accept responsibility or liability for their
         respective privacy policies.
@@ -57,14 +57,14 @@ const PrivacyPolicy: React.FC = () => {
       </p>
 
       <h3>Warranty and Limitation of Liability</h3>
-      <p>
-        THE APPLICATION DEVELOPER PROVIDES THE SOFTWARE AND THE SERVICES "AS IS" WITHOUT
+      <p className={styles.warrantyLimitationText}>
+        The APPLICATION DEVELOPER PROVIDES THE SOFTWARE AND THE SERVICES "AS IS" WITHOUT
         WARRANTY OF ANY KIND EITHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO THE
         IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. ALL RISK OF
         QUALITY AND PERFORMANCE OF THE SOFTWARE OR SERVICES REMAINS WITH YOU.
       </p>
 
-      <p>
+      <p className={styles.warrantyLimitationText}>
         BY USING THIS APPLICATION, YOU AGREE TO THE TERMS PROVIDED IN THIS DISCLAIMER AND THE FOLLOWING
         LIMITATION OF LIABILITY DISCLAIMER. IN NO EVENT WILL THE APPLICATION DEVELOPER, ITS EMPLOYEES,
         DISTRIBUTORS, DIRECTORS OR AGENTS BE LIABLE FOR ANY INDIRECT DAMAGES OR OTHER RELIEF ARISING
@@ -75,7 +75,7 @@ const PrivacyPolicy: React.FC = () => {
         THE POSSIBILITY OF SUCH DAMAGES, OR FOR ANY CLAIM BY ANY OTHER PARTY.
       </p>
 
-      <p>
+      <p className={styles.warrantyLimitationText}>
         BECAUSE SOME STATES OR JURISDICTIONS DO NOT ALLOW THE EXCLUSION OR THE LIMITATION OF LIABILITY FOR CONSEQUENTIAL
         OR INCIDENTAL DAMAGES, IN SUCH STATES OR JURISDICTIONS, THE APPLICATION DEVELOPER'S LIABILITY
         SHALL BE LIMITED TO THE EXTENT PERMITTED BY LAW.
