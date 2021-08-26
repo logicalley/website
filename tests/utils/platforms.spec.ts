e1718ec53d65e34781b5eba078d7f42a23a5130b
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-08-27 00:20:15 +0100

    feat: add tests for platforms

---
+++ tests/utils/platforms.spec.ts
@@ -0,0 +1,9 @@
+import { SupportedPlatformsForLogin } from '../../utils/platforms';
+
+describe('Utils: Platform', (): void => {
+  describe('SupportedPlatformsForLogin', (): void => {
+    test('contains supported platforms for Annie', (): void => {
+      expect(SupportedPlatformsForLogin).toHaveLength(2);
+    });
+  });
+});
