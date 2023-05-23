be1bcb11a4f4f650c806e6b563dbaf88cf887531
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-05-23 02:28:51 +0100

    update copyright

--- components/Content/Footer.tsx
+++ components/Content/Footer.tsx
@@ -4,13 +4,12 @@ import Link from 'next/link';
 import styles from '../TrackDetail/styles.module.css';
 
 const TrackDetailFooter: React.FC = () => {
-  const currentYear: number = new Date().getFullYear();
   const termsOfUseLink: string = '/terms-conditions';
   const privacyPolicyLink: string = '/privacy-policy';
 
   return (
     <footer className={styles.trackFooter}>
-      <span>Copyright © {currentYear} Annie. All Rights Reserved </span>
+      <span>Copyright © 2021 - present Annie. All Rights Reserved </span>
       <br />
       <div className={styles.footerLinks}>
         <Link href={termsOfUseLink} className={styles.platformLink}>
